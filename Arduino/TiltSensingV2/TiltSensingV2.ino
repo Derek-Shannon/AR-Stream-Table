0445d400470edb11e2c36024f6f6f402acedaaa8
Derek-Shannon <shannonderek56@gmail.com>
2026-02-02 18:24:27 -0600

    Create TiltSensingV2.ino

---
+++ Arduino/TiltSensingV2/TiltSensingV2.ino
@@ -0,0 +1,29 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Initialize the sensor
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) { delay(10); }
+  }
+
+  Serial.println("MPU6050 Found!");
+}
+
+void loop() {
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  /* Print out the values */
+  Serial.print("Accel X: "); Serial.print(a.acceleration.x);
+  Serial.print(", Y: "); Serial.print(a.acceleration.y);
+  Serial.print(", Z: "); Serial.println(a.acceleration.z);
+
+  delay(500);
+}
