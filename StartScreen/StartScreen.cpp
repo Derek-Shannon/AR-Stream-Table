b600a6f5948cfb0a2d4970b54e6c058025c0efa5
streamy-desktop <streamydesktop@gmail.com>
2026-04-20 19:00:30 -0500

    Start UI can be scaled to fit screen size
    Font fails to scale

--- StartScreen/StartScreen.cpp
+++ StartScreen/StartScreen.cpp
@@ -52,13 +52,13 @@ int main() {
     unsigned long colorButtonHover = allocColor(display,"#3262b4", WhitePixel(display,screen));
     unsigned long colorBorder = allocColor(display,"#2d3f64", WhitePixel(display,screen));
     unsigned long colorText = allocColor(display,"#ecf2ff", WhitePixel(display,screen));
-    unsigned long colorSubtleText = allocColor(display,"#9db3d9", WhitePixel(display,screen));
-    unsigned long colorPanel = allocColor(display,"#121a2b", WhitePixel(display,screen));
     unsigned long colorAccent = allocColor(display,"#4cc9f0", WhitePixel(display,screen));
 
-    // Reduced height slightly since we removed two checkboxes
-    int winWidth = 650;
-    int winHeight = 220; 
+    int baseWidth = 650;
+    int baseHeight = 220;
+    
+    int winWidth = baseWidth;
+    int winHeight = baseHeight; 
 
     Window window = XCreateSimpleWindow(
         display,
@@ -72,104 +72,112 @@ int main() {
 
     XStoreName(display, window, "SARndbox Startup");
 
+    // Added StructureNotifyMask to listen for resize events
     XSelectInput(display, window,
-        ExposureMask | ButtonPressMask | PointerMotionMask);
+        ExposureMask | ButtonPressMask | PointerMotionMask | StructureNotifyMask);
 
     XMapWindow(display, window);
 
     GC gc = XCreateGC(display, window, 0, nullptr);
     
-    XFontStruct* font = XLoadQueryFont(display, "fixed");
+    XFontStruct* font = XLoadQueryFont(display, "10x20");
     if (font) {
         XSetFont(display, gc, font->fid);
     }
 
-    // Layout Anchors
-    int leftColumnX = 20;
-    int rightColumnX = 350;
-    int itemsStartY = 50;
-
-    // --- LEFT COLUMN: Calibration ---
-
-    // Calibrate Button setup
-    Rect calibrateButton;
-    calibrateButton.w = 200;
-    calibrateButton.h = 25; 
-    calibrateButton.x = leftColumnX; 
-    calibrateButton.y = itemsStartY; 
-
+    // Interactive state flags
     bool calibrateHover = false;
-    std::string calibrateLabel = "Projector Calibration";
-
-    // Kinect Button setup
-    Rect kinectButton;
-    kinectButton.w = 200;
-    kinectButton.h = 25; 
-    kinectButton.x = leftColumnX; 
-    kinectButton.y = itemsStartY + 35; // Placed below Projector Calib
-
     bool kinectHover = false;
+    bool FPVflagHover = false, FPVflagChecked = false;
+    bool heavyRainHover = false, heavyRainChecked = false;
+    bool snowMeltHover = false, snowMeltChecked = false;
+    bool hover = false; // Start button hover
+
+    // Rectangles for UI elements
+    Rect calibrateButton, kinectButton, FPVflag, heavyRainFlag, snowMeltFlag, button;
+
+    // Labels
+    std::string calibrateLabel = "Projector Calibration";
     std::string kinectLabel = "Kinect Calibration";
-
-    // --- RIGHT COLUMN: Operation Settings ---
-
-    // Debug Mode Flag
-    Rect FPVflag;
-    FPVflag.w = 10;
-    FPVflag.h = 10;
-    FPVflag.x = rightColumnX;
-    FPVflag.y = itemsStartY; // Moved to the top of the right column
-
-    bool FPVflagHover = false;
-    bool FPVflagChecked = false;
     std::string FPVflagLabel = "Debug Mode";
-
-    // Heavy Rain Flag
-    Rect heavyRainFlag;
-    heavyRainFlag.w = 10;
-    heavyRainFlag.h = 10;
-    heavyRainFlag.x = rightColumnX;
-    heavyRainFlag.y = FPVflag.y + 25; 
-
-    bool heavyRainHover = false;
-    bool heavyRainChecked = false;
     std::string heavyRainLabel = "Heavy Rain Flag";
-
-    // Snow Melt Flag
-    Rect snowMeltFlag;
-    snowMeltFlag.w = 10;
-    snowMeltFlag.h = 10;
-    snowMeltFlag.x = rightColumnX;
-    snowMeltFlag.y = heavyRainFlag.y + 25; 
-
-    bool snowMeltHover = false;
-    bool snowMeltChecked = false;
     std::string snowMeltLabel = "Snow Melt Flag";
-
-    // Start Button setup (Bottom Right)
-    Rect button;
-    button.w = 270;
-    button.h = 50;
-    button.x = rightColumnX;
-    button.y = snowMeltFlag.y + 35; // Placed below the last checkbox
-
-    bool hover = false;
     std::string label = "Start Projection";
-
-    // Titles
     std::string titleLeft = "Initial Calibration Options";
     std::string titleRight = "Operation Settings";
+
+    // Layout variables updated by resizing
+    int leftColumnX, rightColumnX, titleY;
+
+    // Function to recalculate layout based on window size
+    auto updateLayout = [&]() {
+        float scaleX = (float)winWidth / (float)baseWidth;
+        float scaleY = (float)winHeight / (float)baseHeight;
+
+        leftColumnX = 20 * scaleX;
+        rightColumnX = 350 * scaleX;
+        int itemsStartY = 50 * scaleY;
+        titleY = 30 * scaleY;
+
+        // Calibrate Button
+        calibrateButton.w = 200 * scaleX;
+        calibrateButton.h = 25 * scaleY; 
+        calibrateButton.x = leftColumnX; 
+        calibrateButton.y = itemsStartY; 
+
+        // Kinect Button
+        kinectButton.w = 200 * scaleX;
+        kinectButton.h = 25 * scaleY; 
+        kinectButton.x = leftColumnX; 
+        kinectButton.y = itemsStartY + (35 * scaleY);
+
+        // Checkboxes (kept square using scaleX for uniformity)
+        int boxSize = 10 * scaleX;
+        if (boxSize < 10) boxSize = 10; // min size
+
+        FPVflag.w = boxSize;
+        FPVflag.h = boxSize;
+        FPVflag.x = rightColumnX;
+        FPVflag.y = itemsStartY;
+
+        heavyRainFlag.w = boxSize;
+        heavyRainFlag.h = boxSize;
+        heavyRainFlag.x = rightColumnX;
+        heavyRainFlag.y = FPVflag.y + (25 * scaleY); 
+
+        snowMeltFlag.w = boxSize;
+        snowMeltFlag.h = boxSize;
+        snowMeltFlag.x = rightColumnX;
+        snowMeltFlag.y = heavyRainFlag.y + (25 * scaleY); 
+
+        // Start Button
+        button.w = 270 * scaleX;
+        button.h = 50 * scaleY;
+        button.x = rightColumnX;
+        button.y = snowMeltFlag.y + (35 * scaleY);
+    };
+
+    // Initial layout calculation
+    updateLayout();
 
     while (true) {
         XEvent event;
         XNextEvent(display, &event);
 
-        if (event.type == Expose) {
+        if (event.type == ConfigureNotify) {
+            // Update window dimensions when resized
+            if (event.xconfigure.width != winWidth || event.xconfigure.height != winHeight) {
+                winWidth = event.xconfigure.width;
+                winHeight = event.xconfigure.height;
+                updateLayout();
+            }
+        }
+        else if (event.type == Expose) {
             
             // Draw Titles
             XSetForeground(display, gc, colorAccent);
-            XDrawString(display, window, gc, leftColumnX, 30, titleLeft.c_str(), titleLeft.length());
-            XDrawString(display, window, gc, rightColumnX, 30, titleRight.c_str(), titleRight.length());
+            XDrawString(display, window, gc, leftColumnX, titleY, titleLeft.c_str(), titleLeft.length());
+            XDrawString(display, window, gc, rightColumnX, titleY, titleRight.c_str(), titleRight.length());
 
             // --- START BUTTON ---
             XSetForeground(display, gc, hover ? colorButtonHover : colorButton);
@@ -207,113 +215,62 @@ int main() {
             XSetForeground(display, gc, colorText);
             XDrawString(display, window, gc, kTextX, kTextY, kinectLabel.c_str(), kinectLabel.length());
 
-            // --- DEBUG FLAG ---
-            XSetForeground(display, gc, FPVflagHover ? colorButtonHover : colorButton);
-            XFillRectangle(display, window, gc, FPVflag.x, FPVflag.y, FPVflag.w, FPVflag.h);
-            XSetForeground(display, gc, colorBorder);
-            XDrawRectangle(display, window, gc, FPVflag.x, FPVflag.y, FPVflag.w, FPVflag.h);
+            // Helper lambda for checkboxes
+            auto drawCheckbox = [&](Rect& rect, bool isHover, bool isChecked, std::string lbl) {
+                XSetForeground(display, gc, isHover ? colorButtonHover : colorButton);
+                XFillRectangle(display, window, gc, rect.x, rect.y, rect.w, rect.h);
+                XSetForeground(display, gc, colorBorder);
+                XDrawRectangle(display, window, gc, rect.x, rect.y, rect.w, rect.h);
 
-            int ftx = FPVflag.x + 15;
-            int fty = FPVflag.y + 9;
-            XSetForeground(display, gc, colorText);
-            XDrawString(display, window, gc, ftx, fty, FPVflagLabel.c_str(), FPVflagLabel.length());
-
-            if (FPVflagChecked) {
-                const char* mark = "x";
-                ftx = FPVflag.x + 3;
-                fty = FPVflag.y + 8;
+                // Text Label offset scaled relative to box
+                int tx = rect.x + rect.w + 5; 
+                int ty = rect.y + rect.h - 2;
                 XSetForeground(display, gc, colorText);
-                XDrawString(display, window, gc, ftx, fty, mark, 1);
-            }
+                XDrawString(display, window, gc, tx, ty, lbl.c_str(), lbl.length());
 
-            // --- HEAVY RAIN FLAG ---
-            XSetForeground(display, gc, heavyRainHover ? colorButtonHover : colorButton);
-            XFillRectangle(display, window, gc, heavyRainFlag.x, heavyRainFlag.y, heavyRainFlag.w, heavyRainFlag.h);
-            XSetForeground(display, gc, colorBorder);
-            XDrawRectangle(display, window, gc, heavyRainFlag.x, heavyRainFlag.y, heavyRainFlag.w, heavyRainFlag.h);
+                if (isChecked) {
+                    const char* mark = "x";
+                    int markX = rect.x + (rect.w / 2) - 3;
+                    int markY = rect.y + (rect.h / 2) + 4;
+                    XSetForeground(display, gc, colorText);
+                    XDrawString(display, window, gc, markX, markY, mark, 1);
+                }
+            };
 
-            int htx = heavyRainFlag.x + 15;
-            int hty = heavyRainFlag.y + 9;
-            XSetForeground(display, gc, colorText);
-            XDrawString(display, window, gc, htx, hty, heavyRainLabel.c_str(), heavyRainLabel.length());
-
-            if (heavyRainChecked) {
-                const char* mark = "x";
-                htx = heavyRainFlag.x + 3;
-                hty = heavyRainFlag.y + 8;
-                XSetForeground(display, gc, colorText);
-                XDrawString(display, window, gc, htx, hty, mark, 1);
-            }
-
-            // --- SNOW MELT FLAG ---
-            XSetForeground(display, gc, snowMeltHover ? colorButtonHover : colorButton);
-            XFillRectangle(display, window, gc, snowMeltFlag.x, snowMeltFlag.y, snowMeltFlag.w, snowMeltFlag.h);
-            XSetForeground(display, gc, colorBorder);
-            XDrawRectangle(display, window, gc, snowMeltFlag.x, snowMeltFlag.y, snowMeltFlag.w, snowMeltFlag.h);
-
-            int stx = snowMeltFlag.x + 15;
-            int sty = snowMeltFlag.y + 9;
-            XSetForeground(display, gc, colorText);
-            XDrawString(display, window, gc, stx, sty, snowMeltLabel.c_str(), snowMeltLabel.length());
-
-            if (snowMeltChecked) {
-                const char* mark = "x";
-                stx = snowMeltFlag.x + 3;
-                sty = snowMeltFlag.y + 8;
-                XSetForeground(display, gc, colorText);
-                XDrawString(display, window, gc, stx, sty, mark, 1);
-            }
+            // Draw Checkboxes
+            drawCheckbox(FPVflag, FPVflagHover, FPVflagChecked, FPVflagLabel);
+            drawCheckbox(heavyRainFlag, heavyRainHover, heavyRainChecked, heavyRainLabel);
+            drawCheckbox(snowMeltFlag, snowMeltHover, snowMeltChecked, snowMeltLabel);
         }
 
         else if (event.type == MotionNotify) {
             int mx = event.xmotion.x;
             int my = event.xmotion.y;
 
-            bool newHover = button.contains(mx, my);
-            if (newHover != hover) {
-                hover = newHover;
-                XEvent redraw;
-                redraw.type = Expose;
-                XSendEvent(display, window, False, ExposureMask, &redraw);
-            }
-            newHover = calibrateButton.contains(mx, my);
-            if (newHover != calibrateHover) {
-                calibrateHover = newHover;
-                XEvent redraw;
-                redraw.type = Expose;
-                XSendEvent(display, window, False, ExposureMask, &redraw);
-            }
-            newHover = FPVflag.contains(mx, my);
-            if (newHover != FPVflagHover) {
-                FPVflagHover = newHover;
-                XEvent redraw;
-                redraw.type = Expose;
-                XSendEvent(display, window, False, ExposureMask, &redraw);
-            }
-            newHover = heavyRainFlag.contains(mx, my);
-            if (newHover != heavyRainHover) {
-                heavyRainHover = newHover;
-                XEvent redraw;
-                redraw.type = Expose;
-                XSendEvent(display, window, False, ExposureMask, &redraw);
-            }
-            newHover = kinectButton.contains(mx, my);
-            if (newHover != kinectHover) {
-                kinectHover = newHover;
-                XEvent redraw;
-                redraw.type = Expose;
-                XSendEvent(display, window, False, ExposureMask, &redraw);
-            }
-            newHover = snowMeltFlag.contains(mx, my);
-            if (newHover != snowMeltHover) {
-                snowMeltHover = newHover;
+            bool redrawNeeded = false;
+
+            auto checkHover = [&](Rect& rect, bool& currentHover) {
+                bool newHover = rect.contains(mx, my);
+                if (newHover != currentHover) {
+                    currentHover = newHover;
+                    redrawNeeded = true;
+                }
+            };
+
+            checkHover(button, hover);
+            checkHover(calibrateButton, calibrateHover);
+            checkHover(kinectButton, kinectHover);
+            checkHover(FPVflag, FPVflagHover);
+            checkHover(heavyRainFlag, heavyRainHover);
+            checkHover(snowMeltFlag, snowMeltHover);
+
+            if (redrawNeeded) {
                 XEvent redraw;
                 redraw.type = Expose;
                 XSendEvent(display, window, False, ExposureMask, &redraw);
             }
         }
 
-        // button events yipee!
         else if (event.type == ButtonPress) {
             int mx = event.xbutton.x;
             int my = event.xbutton.y;
@@ -321,9 +278,9 @@ int main() {
             if (button.contains(mx, my)) {
                 std::string cmdLine = "cd "+getExecutableDir()+"/../arsandbox/bin && ./SARndbox -fpv";
 
-                if (FPVflagChecked == true) cmdLine += " -debug";
-                if (heavyRainChecked == true) cmdLine += " -rs .75";
-                if (snowMeltChecked == true) cmdLine += " -sm 0";
+                if (FPVflagChecked) cmdLine += " -debug";
+                if (heavyRainChecked) cmdLine += " -rs .75";
+                if (snowMeltChecked) cmdLine += " -sm 0";
                 
                 std::cout << cmdLine << std::endl;
                 
